f344ae51660e3a64c6cb0daa7e1ff15078eb8a3c
Doy Kim <kimdee.dev@gmail.com>
2022-09-01 12:08:36 +0900

    Initial Commit

---
+++ MemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoProject
+//
+//  Created by Doy Kim on 2022/09/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
