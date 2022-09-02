f7ac086186b3e5fcc0b7937bbdead0db381c45cd
Doy Kim <kimdee.dev@gmail.com>
2022-09-02 20:00:21 +0900

    [Design] - #3 뷰에 테이블뷰 추가

--- MemoProject/Presentation/List/ListViewController.swift
+++ MemoProject/Presentation/List/ListViewController.swift
@@ -6,48 +6,79 @@
 //
 
 import UIKit
-
-import UIKit
+import SnapKit
+import Then
 
 class ListViewController: UIViewController {
     // MARK: - Properties
-    
+    lazy var tableView = UITableView().then {
+        $0.delegate = self
+        $0.dataSource = self
+        $0.backgroundColor = .clear
+    }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
+        setupNavigationBar()
         setConstraints()
+        
 
     }
     // MARK: - Actions
     
     
     // MARK: - Helpers
-    
     func setupUI() {
         /// View
         self.view.backgroundColor = .systemBackground
+        
+        view.addSubview(tableView)
+    }
+    
+    func setupNavigationBar() {
+        
         /// Navigation Item
         /// - Title
-
         self.navigationItem.title = "0개의 메모"
+        /// -- 타이틀을 크게 설정
         self.navigationController?.navigationBar.prefersLargeTitles = true
-        
         
         /// - Search Controller
         let searchController = UISearchController(searchResultsController: nil)
         searchController.searchBar.placeholder = "검색"
         
         navigationItem.searchController = searchController
+        /// -- 스크롤이 되더라도 검색바가 보이게 설정
         navigationItem.hidesSearchBarWhenScrolling = false
     }
     func setConstraints() {
-        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
 
 
 
+}
+
+// MARK: - Table View
+extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    /// 섹션 개수
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    /// 셀 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    /// 셀 구성
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
