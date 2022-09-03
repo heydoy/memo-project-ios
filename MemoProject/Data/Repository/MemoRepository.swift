f9f3529639295a246abb9fa5e7864510d68cc8b0
Doy Kim <kimdee.dev@gmail.com>
2022-09-03 20:24:39 +0900

    [Feature] - #9 레포지토리 패턴으로 메모 CRUD 레포지토리 선언

---
+++ MemoProject/Data/Repository/MemoRepository.swift
@@ -0,0 +1,66 @@
+//
+//  MemoRepository.swift
+//  MemoProject
+//
+//  Created by Doy Kim on 2022/09/03.
+//
+
+import Foundation
+import RealmSwift
+
+protocol MemoRepositoryType {
+    func fetch() -> Results<Memo> //항상 최신순으로 가져오기
+    func fetchFilter(_ query: String) -> Results<Memo>
+    func createMemo(_ item: Memo)
+    func updateMemo(_ item: Memo)
+    func deleteMemo(_ item: Memo)
+}
+
+class MemoRepository: MemoRepositoryType {
+    let localRealm = try! Realm()
+    
+    func fetch() -> Results<Memo> {
+        return localRealm.objects(Memo.self).sorted(byKeyPath: "dateCreated", ascending: false) // 항상 최신순으로 가져온다
+    }
+    
+    func fetchFilter(_ query: String) -> Results<Memo> {
+        let tasks = localRealm.objects(Memo.self)
+        let results = tasks.where {
+            $0.title.contains(query) || $0.content.contains(query)
+        }
+        
+        return results
+    }
+    
+    func createMemo(_ item: Memo) {
+        do {
+            try localRealm.write  {
+                localRealm.add(item)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func updateMemo(_ item: Memo) {
+        do {
+            try self.localRealm.write {
+                localRealm.add(item, update: .modified)
+                
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    func deleteMemo(_ item: Memo) {
+        do {
+            try localRealm.write {
+                localRealm.delete(item)
+            }
+        } catch let error {
+            print(error)
+        }
+    }
+
+}
--- MemoProject/Domain/Model/Memo.swift
+++ MemoProject/Domain/Model/Memo.swift
@@ -12,8 +12,7 @@ class Memo: Object {
     @Persisted(primaryKey: true) var _id: ObjectId
     @Persisted(indexed: true) var title: String
     @Persisted var content: String? // 내용이 없을 수도 있음
-    @Persisted var dateCreated = Date() // 최초작성시각
-    @Persisted var dateModified = Date() // 수정한 시각
+    @Persisted var dateCreated = Date() // 작성시각
     @Persisted var isPinned: Bool // 고정메모 여부
     
     convenience init(title: String, content: String?, dateCreated: Date, dateModified: Date ) {
@@ -22,7 +21,6 @@ class Memo: Object {
         self.title = title
         self.content = content
         self.dateCreated = dateCreated
-        self.dateModified = dateModified
         self.isPinned = false
     }
 }
